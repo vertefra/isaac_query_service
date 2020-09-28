d0d90253946626a7ad5d98c8593c77b48711c741
vertefra <verte.fra@gmail.com>
2020-09-28 09:27:14 -0400

    isaac can create a user when receive the uvent userSignUp

--- models/Converation.js
+++ models/Converation.js
@@ -1,22 +1,27 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const ConversationSchema = new Schema(
-    {
-        username : { type: String, required: true, unique=true},
-        received_messages: [{
-                sender_username: {type: String, required: true},
-                message: {type: String },
-                timestamp: {type: Date}
-        }],
-        sent_messages: [{
-            recipient_username: {type: String, required: true},
-            message: {type:String},
-            timestamp: {type: Date}
-        }]
-    },{timestamps: true}
-)
-const Conversation = mongoose.model("Conversation", ConversationSchema)
+  {
+    username: { type: String, required: true, unique: true },
+    received_messages: [
+      {
+        sender_username: { type: String, required: true },
+        message: { type: String },
+        timestamp: { type: Date },
+      },
+    ],
+    sent_messages: [
+      {
+        recipient_username: { type: String, required: true },
+        message: { type: String },
+        timestamp: { type: Date },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+const Conversation = mongoose.model("Conversation", ConversationSchema);
 
-module.exports = Conversation
+module.exports = Conversation;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
 const app = express();
+const Conversation = require("./models/Converation");
 const { MONGO_USR, MONGO_PSW, MONGO_DB } = process.env;
 const MONGO_URI = `mongodb+srv://${MONGO_USR}:${MONGO_PSW}@cluster0-fg0dv.gcp.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`;
 
@@ -32,10 +33,27 @@ app.use(express.json());
 
 // events listeners
 
-app.post(`${carol_bus}/events`, (req, res) => {
+app.post(`/events`, (req, res) => {
   console.log("received event from Carol: ", req.body);
+  switch (req.body.type) {
+    case "userSignUp":
+      const { signUpUsername: username } = req.body.payload;
+      Conversation.create({ username }, (err, createdUser) => {
+        createdUser
+          ? res
+              .status(201)
+              .json({ user_history_created: true, user: createdUser })
+          : res
+              .status(500)
+              .json({ error: "failed creating resource", message: err });
+      });
+      break;
+    case "messageSent":
+      break;
+  }
+  res.status(200).json({ status: "ok" });
 });
 
-app.listen(process.env.PORT || 3005, () => {
+app.listen(process.env.PORT || 3002, () => {
   console.log("Isaac is listening on 3002");
 });
