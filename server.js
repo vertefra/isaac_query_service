7a00e0ff4ebbeddd65cc19f819319e34161832d4
vertefra <verte.fra@gmail.com>
2020-09-28 07:45:12 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+const app = express();
+const { MONGO_USR, MONGO_PSW, MONGO_DB } = process.env;
+const MONGO_URI = `mongodb+srv://${MONGO_USR}:${MONGO_PSW}@cluster0-fg0dv.gcp.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`;
+
+// SERVICES
+
+const carol_bus = "http://127.0.0.1:3005";
+
+// Mongo connection
+
+console.log(MONGO_URI);
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("open", () => {
+  console.log("Mongo is connected with Isaac");
+  console.log(`DB: ${MONGO_DB}`);
+});
+
+// middlewares
+
+app.use(cors());
+app.use(express.json());
+
+// listeners
+
+app.listen(process.env.PORT || 3005, () => {
+  console.log("Isaac is listening on 3002");
+});
